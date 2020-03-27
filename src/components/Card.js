701629e78dbbb7066fbf14d51678e33e2f622b01
Kari Burt <kandreahburt@gmail.com>
2020-03-27 02:12:16 -0500

    First react app

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const card = ({name,email,id}) => {
+    return (
+        <div className='tc bg-light-green dib pa3 ma2 grow bw2 shadow-5 dib'>
+            <img alt='robots' src={`https://robohash.org/${id}`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default card;
